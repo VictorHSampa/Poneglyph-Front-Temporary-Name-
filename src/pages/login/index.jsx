1dbfecf4da09db12ab58d40e12242a6a981ac8a8
VictorHSampa <victorhugosampa@gmail.com>
2026-02-24 10:53:45 -0300

    Rascunho de tela de login

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,44 @@
+import { use, useState, useRef } from 'react'
+import viteLogo from '/vite.svg'
+import './style.css'
+import Trash from '../../assets/trash.svg'
+import api from '../../services/api'
+import { useEffect } from 'react'
+
+
+function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post('/user/login', {
+        email,
+        password
+      })
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1>Welcome to Poneglyph</h1>
+      <form>
+        <h1>Login</h1>
+        <input name='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input name='password' placeholder='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type='button' onClick={handleLogin}>Login</button>
+      </form>
+
+      <div className='register'>
+        <h1>Don't have an account?</h1>
+        <button type='button'>Register</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
