1cf3278e9f07eac88a0ff9751cc83f7d8302a687
VictorHSampa <victorhugosampa@gmail.com>
2026-03-01 02:01:34 -0300

    Criação da identidade visual da página de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,6 +2,7 @@ import { use, useState, useRef } from 'react'
 import './style.css'
 import api from '../../services/api'
 import { useEffect } from 'react'
+import LoginImage from '../../assets/uta.webp'
 
 
 function Login() {
@@ -22,21 +23,21 @@ function Login() {
 
   return (
     <div className='container'>
-      <form className = 'Login-form'>
-        <div className = 'login-header'>
-        <h1>WELCOME BACK</h1>
-        <p>Please enter your details</p>
+      <form className='Login-form'>
+        <div className='login-header'>
+          <h1>WELCOME BACK</h1>
+          <p>Please enter your details</p>
         </div>
         <p>Email</p>
-        <input name='email' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input name='email' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <p>Password</p>
-        <input name='password' placeholder='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <input name='password' placeholder='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type='button' onClick={handleLogin}>Sign in</button>
-        <p className = 'register'>Don't have an account?<a href='/register'> Sign up for free!</a></p>
+        <p className='register'>Don't have an account?<a href='/register'> Sign up for free!</a></p>
       </form>
-       
-      
-
+      <div className='login-image'>
+          <img src={LoginImage} alt='Login Image' />
+        </div>
     </div>
   )
 }
